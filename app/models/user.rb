c05a33926c9d80092d92c6772b38ac3873a9b96d
Brent <karmapoint@gmail.com>
2017-03-14 16:32:06 -0700

    fix plural users problem

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ActiveRecord::Base
 
 	attr_reader :password
 
-	validates :username, :password_digest, :session_token, presence: true
-	validates :username, uniqueness: true
+	validates :email, :password_digest, :session_token, presence: true
+	validates :email, uniqueness: true
 	validates :password, length: {minimum: 6}, allow_nil: :true
 
 	after_initialize :ensure_session_token
@@ -15,8 +15,8 @@ class User < ActiveRecord::Base
 		@password = password
 	end
 
-	def self.find_by_credentials username, password
-		user = User.find_by(username: username)
+	def self.find_by_credentials email, password
+		user = User.find_by(email: email)
 		return nil unless user
 		user.password_is?(password) ? user : nil
 	end
