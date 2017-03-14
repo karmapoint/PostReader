eabe8cf02af522e4b0f55fa81edb5ad2dc9479ca
Brent <karmapoint@gmail.com>
2017-03-14 16:02:12 -0700

    fix user model and controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,33 +1,21 @@
-class ApplicationController < ActionController::Base
+class Api::UsersController < ApplicationController
 
-  protect_from_forgery with: :exception
-  helper_method :current_user, :logged_in?
+	def create
+		# debugger
+		@user = User.new(user_params)
 
-  private
+		if @user.save
+			login(@user)
+			render "api/users/show"
+		else
+			render json: @user.errors.full_messages, status: 422
+		end
+	end
 
-  def current_user
-    return nil unless session[:session_token]
-    @current_user ||= User.find_by(session_token: session[:session_token])
-  end
+	private
 
-  def logged_in?
-    !!current_user
-  end
-
-  def login(user)
-    user.reset_session_token!
-    session[:session_token] = user.session_token
-    @current_user = user
-  end
-
-  def logout
-    current_user.reset_session_token!
-    session[:session_token] = nil
-    @current_user = nil
-  end
-
-  def require_logged_in
-    render json: {base: ['invalid credentials']}, status: 401 if !current_user
-  end
+	def user_params
+		params.require(:user).permit(:username, :password)
+	end
 
 end
--- frontend/index.jsx
+++ frontend/index.jsx
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom';
 //Components
 import Root from './components/root';
 import configureStore from './store/store';
+import * as API from './util/session_api_util';
 
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -17,6 +18,13 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   window.store = store;
+  window.signup = API.signup;
+  window.login = API.login;
+  window.logout = API.logout;
+
+
+
+
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={store}/>, root);
 });
