0ebf50d82127c05fc910eda2f05819ed9fa78a87
Brent <karmapoint@gmail.com>
2017-03-14 17:40:03 -0700

    add forms - frontend auth is functional

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -6,6 +6,7 @@ import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
 // react components
 import App from './app';
+import SessionFormContainer from './session_form/session_form_container';
 
 const Root = ({ store }) => {
 
@@ -13,6 +14,8 @@ const Root = ({ store }) => {
     <Provider store={store}>
       <Router history={ hashHistory }>
         <Route path="/" component={ App } />
+          <Route path="/login" component={ SessionFormContainer } />
+          <Route path="/signup" component={ SessionFormContainer } />
       </Router>
     </Provider>
   );
