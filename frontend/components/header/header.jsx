938e6809e5f83749e272d4c246dc099ec663d9d4
Brent <karmapoint@gmail.com>
2017-03-15 17:47:22 -0700

    style header

--- frontend/components/header/header.jsx
+++ frontend/components/header/header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link, hashHistory } from 'react-router';
 
 
-
+// Look at this when working on homepage
 const sessionLinks = () => (
   <header className="content-header">
     <Link to="/"><img src="https://res.cloudinary.com/postreader/image/upload/v1489611558/postreader_content_logo_r1atgy.png" alt="PostReader Logo"></img> <span className="logo">PostReader</span></Link>
@@ -17,16 +17,18 @@ const sessionLinks = () => (
 const personalGreeting = (currentUser, logout) => (
   <header className="content-header">
     <div className="logo-block">
-    <Link to="/">
+    <Link to="/home">
       <img src="https://res.cloudinary.com/postreader/image/upload/v1489611558/postreader_content_logo_r1atgy.png" alt="PostReader Logo"></img>
     </Link>
-    <Link to="/" className="logo">
-      PostReader
-    </Link>
+      <h1><Link to="/home" className="logo">PostReader</Link></h1>
     </div>
+
     <div className="user-logout">
-      <h2 className="header-user">Hi, {currentUser.email}!</h2>
-      <button className="logout-button" onClick={() => logout().then(() => hashHistory.replace("/login"))}>Log Out</button>
+      <h3 className="header-user">Welcome {currentUser.email}</h3>
+      <button className="logout-button"
+        onClick={() => logout().then(() => hashHistory.replace("/login"))}>
+        Log Out
+      </button>
     </div>
 	</header>
 );
