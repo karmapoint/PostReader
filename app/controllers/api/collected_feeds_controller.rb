8d405692ff2d7982533e7a716c533d676c27566d
Brent <karmapoint@gmail.com>
2017-03-20 21:12:15 -0700

    fix missing end in collected_feeds controller

--- app/controllers/api/collected_feeds_controller.rb
+++ app/controllers/api/collected_feeds_controller.rb
@@ -4,6 +4,7 @@ class Api::FeedsController < ApplicationController
     @collected_feed = CollectedFeed.new(collected_feed_params)
     if @collected_feed.save
       render "api/collections/show/#{@collected_feed.collection_id}"
+    end 
   end
 
   def collected_feeds_params
